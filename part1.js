3b6e414d631a29539ca2a1be5d57f3adeb4724c9
Justin O'Dea <odeajustin@gmail.com>
2024-02-10 11:40:19 -0700

    removed work I do not need

--- part1.js
+++
@@ -1,8 +0,0 @@
-// #8 Define a function to concatenate two strings
-function concatenateNames(firstPart, secondPart) {
-    // Print
-    console.log(firstPart + ' ' + secondPart);
-  }
-  
-  // Call the concatenateNames function with example names
-  concatenateNames("Jane", "Doe");
